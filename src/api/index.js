cb3d664dbd85f1c5dd6e5d6f9282b235b2d41922
Shivam Mahajan <shivam.sm2002@gmail.com>
2022-12-25 23:15:29 +0530

    Fixed api error

--- src/api/index.js
+++ src/api/index.js
@@ -4,14 +4,14 @@ export const getCodeforcesData = () =>
   axios.get("https://codeforces.com/api/user.info?handles=Cursed_Ninja");
 export const getCodechefData = () =>
   axios.get(
-    "https://competitive-coding-api.herokuapp.com/api/codechef/shivam23434545"
+    "https://competitive-programming.onrender.com/api/codechef/shivam23434545"
   );
 export const getAtcoderData = () =>
   axios.get(
-    "https://competitive-coding-api.herokuapp.com/api/atcoder/Cursed_Ninja"
+    "https://competitive-programming.onrender.com/api/atcoder/Cursed_Ninja"
   );
 
 export const getLeetcodeData = () =>
   axios.get(
-    "https://competitive-coding-api.herokuapp.com/api/leetcode/Cursed_Ninja"
+    "https://competitive-programming.onrender.com/api/leetcode/Cursed_Ninja"
   );
