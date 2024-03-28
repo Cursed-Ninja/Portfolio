947c86c91ea070c7b26011a23d6666840835a85d
Shivam Mahajan <shivam.sm2002@gmail.com>
2024-03-28 20:41:06 +0530

    feat: implement functionality to handle projects without images

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,15 +2,20 @@ import data from "../data/projects.json";
 import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 
 const Projects = () => {
-  const projects = data.map((project, index) => (
-    <div className="image-wrapper" key={`project-${index}`}>
-      <img src={project.image} alt={project.title} />
+  const projects = [...data].reverse().map((project, index) => (
+    <div
+      className={`${!project.image && "no-image"} image-wrapper`}
+      key={`project-${index}`}
+    >
+      {project.image && <img src={project.image} alt={project.title} />}
       <div className="content">
         <h3>{project.title}</h3>
         <p>{project.description}</p>
-        <a href={project.link} target="_blank" rel="noopener noreferrer">
-          Visit website <ChevronRightRoundedIcon sx={{ fontSize: "1rem" }} />
-        </a>
+        {project.link && (
+          <a href={project.link} target="_blank" rel="noopener noreferrer">
+            Visit website <ChevronRightRoundedIcon sx={{ fontSize: "1rem" }} />
+          </a>
+        )}
         <a href={project.github} target="_blank" rel="noopener noreferrer">
           View on Github
           <ChevronRightRoundedIcon sx={{ fontSize: "1rem" }} />
